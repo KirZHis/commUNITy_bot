162c134ffce33d58654fc9f4428f81acc9541b1b
Timaid <120682746+KirZHis@users.noreply.github.com>
2023-06-04 14:46:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ user_states = {}
 """conn = sqlite3.connect('Answers.db')
 cursor = conn.cursor()"""   # Если будет большой поток
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     user_states[message.chat.id] = 0
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Да")
-    btn2 = types.KeyboardButton("Нет")
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Да", callback_data="1")
+    btn2 = types.InlineKeyboardButton("Нет", callback_data="2")
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id,
                      text="Для того чтобы начать работу, скажи, можно ли передать ваш username найденному человеку?".format(
@@ -32,36 +33,37 @@ def help_message(message):
                          message.from_user), reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: message.text == "Нет" and user_states[message.chat.id] == 0)
-def func(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("/start")
-    btn2 = types.KeyboardButton("/help")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id,
-                     text="Простите, но тогда вы не сможете воспользоваться ботом. Начните сначала, если хотите продолжить.".format(
-                         message.from_user), reply_markup=markup)
-
-
-@bot.message_handler(func=lambda message: message.text == "Да"
-                                          and user_states[message.chat.id] == 0)
-def func(message):
-    if message.from_user.username == None:
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data == "1":
+        if call.message.from_user.username == None:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn = types.KeyboardButton("/start")
+            markup.add(btn)
+            bot.send_message(call.message.chat.id,
+                             text="Извините, но для того, чтобы продолжить, вам нужно задать username для вашего аккаунта в Telegram".format(
+                                 call.message.from_user), reply_markup=markup)
+        else:
+            user_states[call.message.chat.id] = 1
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("Окей, давай начнём!")
+            btn2 = types.KeyboardButton("Хорошо, я приду, как потребуется помощь")
+            markup.add(btn1, btn2)
+            bot.send_message(call.message.chat.id,
+                             text="Привет! Ищешь единомышленников для создания стартапа? Хочешь найти команду под проект? Мечтаешь познакомиться с профи, работающими в твоей сфере? Ты на правильном пути! Скорее регистрируйся, я помогу найти нужных людей.".format(
+                                 call.message.from_user), reply_markup=markup)
+    elif call.data == "2":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn = types.KeyboardButton("/start")
-        markup.add(btn)
-        bot.send_message(message.chat.id,
-                         text="Извините, но для того, чтобы продолжить, вам нужно задать username для вашего аккаунта в Telegram".format(
-                             message.from_user), reply_markup=markup)
-    else:
-        user_states[message.chat.id] = 1
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Окей, давай начнём!")
-        btn2 = types.KeyboardButton("Хорошо, я приду, как потребуется помощь")
+        btn1 = types.KeyboardButton("/start")
+        btn2 = types.KeyboardButton("/help")
         markup.add(btn1, btn2)
-        bot.send_message(message.chat.id,
-                        text="Привет! Ищешь единомышленников для создания стартапа? Хочешь найти команду под проект? Мечтаешь познакомиться с профи, работающими в твоей сфере? Ты на правильном пути! Скорее регистрируйся, я помогу найти нужных людей.".format(
-                            message.from_user), reply_markup=markup)
+        bot.send_message(call.message.chat.id,
+                         text="Простите, но тогда вы не сможете воспользоваться ботом. Начните сначала, если хотите продолжить.".format(
+                             call.message.from_user), reply_markup=markup)
+    else:
+        user_info = bot.get_chat(call.data)
+        bot.send_message(call.message.chat.id, f'@{user_info.username}')
+
 
 @bot.message_handler(func=lambda message: message.text == "Хорошо, я приду, как потребуется помощь"
                                           and user_states[message.chat.id] == 1)
@@ -72,6 +74,7 @@ def func(message):
     bot.send_message(message.chat.id,
                      text="Для продолжения нажми кнопку".format(
                          message.from_user), reply_markup=markup)
+
 
 @bot.message_handler(func=lambda message: message.text == "Окей, давай начнём!"
                                           and user_states[message.chat.id] == 1)
@@ -111,6 +114,7 @@ def process_city_step(message, name, age):
                            'Кто тебе интересен? Кого ты ищешь?', reply_markup=markup)
     bot.register_next_step_handler(msg, process_partner_step, name, age, city)
 
+
 def process_partner_step(message, name, age, city):
     partner_prof = message.text
 
@@ -139,12 +143,16 @@ def process_about_step(message, name, age, city, partner_prof):
 def process_profession_step(message, name, age, city, partner_prof, about):
     profession = message.text
 
+    bot.send_message(message.chat.id, "<b>Ваша анкета</b>", parse_mode="HTML")
+    bot.send_message(message.chat.id, f'<u>Имя</u>: {name} \n <u>Возраст</u>: {age} \n <u>Место проживания</u>: {city} \n <u>Ваша профессия</u>: {profession} \n <u>О себе</u>: {about}', parse_mode="HTML")
+
     conn = sqlite3.connect('Answers.db')
     c = conn.cursor()
 
     try:
         c.execute("INSERT INTO Answers (ID, Name, Age, City, About, Profession, State) VALUES (?, ?, ?, ?, ?, ?, ?)",
                   (message.chat.id, name, age, city, about, profession, 0))
+        conn.commit()
     except:
         c.execute('''
                        UPDATE Answers
@@ -162,6 +170,7 @@ def process_profession_step(message, name, age, city, partner_prof, about):
 
     msg = bot.send_message(message.chat.id, "Большое спасибо за ответы. Теперь вы можете найти партнера.", reply_markup=markup)
     bot.register_next_step_handler(msg, find_partner, partner_prof, partner_number = 0)
+
 
 def find_partner(message, partner_prof, partner_number):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -181,7 +190,15 @@ def find_partner(message, partner_prof, partner_number):
         else:
             try:
                 partner = c.fetchone()
-                msg = bot.send_message(message.chat.id, f'Имя: {partner[1]} \n Возраст: {partner[2]} \n Место проживания: - {partner[3]} \n Рассказ о себе: {partner[4]}')
+                for i in range(partner_number):
+                    partner = c.fetchone()
+
+                markup_later = types.InlineKeyboardMarkup()
+                btn_later = types.InlineKeyboardButton("USERNAME", callback_data=partner[0])
+                markup_later.add(btn_later)
+
+                msg = bot.send_message(message.chat.id, f'Имя: {partner[1]} \n Возраст: {partner[2]} \n Место проживания: {partner[3]} \n О себе: {partner[4]}', reply_markup=markup_later)
+
                 bot.register_next_step_handler(msg, find_partner, partner_prof, partner_number=partner_number+1)
             except TypeError:
                 msg = bot.send_message(message.chat.id, 'Извините, напарник не найден. Просим набраться терпения. Или попробуйте еще раз.')
@@ -192,6 +209,7 @@ def find_partner(message, partner_prof, partner_number):
                         SET State = 1
                         WHERE ID = ?
                     ''', (message.chat.id,))
+        conn.commit()
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn = types.KeyboardButton("Стать видимым")
@@ -205,6 +223,7 @@ def find_partner(message, partner_prof, partner_number):
                         SET State = 0
                         WHERE ID = ?
                     ''', (message.chat.id,))
+        conn.commit()
 
         msg = bot.send_message(message.chat.id, "Поиск снова включен", reply_markup=markup)
         bot.register_next_step_handler(msg, find_partner, partner_prof, partner_number)
@@ -221,12 +240,14 @@ def start_find(message, partner_prof, partner_number):
                     SET State = 0
                     WHERE ID = ?
                 ''', (message.chat.id,))
+    conn.commit()
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn = types.KeyboardButton("Стать видимым")
     markup.add(btn)
     msg = bot.send_message(message.chat.id, "Вы не будете видны другим пользователям",
                            reply_markup=markup)
     bot.register_next_step_handler(msg, find_partner, partner_prof, partner_number)
-    
-    
+
+
 bot.infinity_polling()
